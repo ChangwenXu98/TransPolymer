65b0a3b6e680a1d42aa3a7d6567e8b0f01bac528
Himanshi Yadav <hyadav@midway3-login3.rcc.local>
2023-10-17 14:14:50 -0500

    More RDKit images added

--- demo_example.py
+++ demo_example.py
@@ -1,11 +1,16 @@
 # import RDKit ----------------------------------------------------------------
 from rdkit import Chem
 from rdkit.Chem import Draw
+from rdkit.Chem import rdMolDescriptors
+drawOptions = Draw.rdMolDraw2D.MolDrawOptions()
+drawOptions.prepareMolsBeforeDrawing = False
+from rdkit.Chem.Draw import IPythonConsole
+import pdb
 
 # define the smiles string and covert it into a molecule sturcture ------------
-caffeine_smiles = '[Li+].C(F)(F)(F)S(=O)(=O)[N-]S(=O)(=O)C(F)(F)F'
+smiles = '[Li+].C(F)(F)(F)S(=O)(=O)[N-]S(=O)(=O)C(F)(F)F'
 
-mol = Chem.MolFromSmiles(caffeine_smiles)
+mol = Chem.MolFromSmiles(smiles)
 
 # draw the modecule -----------------------------------------------------------
 Draw.MolToFile(mol, 'liq_2.png')
@@ -13,5 +18,15 @@ Draw.MolToFile(mol, 'liq_2.png')
 # draw the molecule with property ---------------------------------------------
 for i, atom in enumerate(mol.GetAtoms()):
     atom.SetProp("molAtomMapNumber", str(atom.GetIdx()))
+
+#draw Morgan Fingerprint
+
+bi = {}
+fp = rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, radius=2, bitInfo=bi)
+
+tpls = [(mol, x, bi) for x in fp.GetOnBits()]
+
+p = Draw.DrawMorganBits(tpls, molsPerRow=5, legends=[str(x) for x in fp.GetOnBits()], drawOptions=drawOptions)
+p.save('liq_2_morgan.png')
     
-Draw.MolToFile(mol, 'liq_2_with_prop.png')
+# Draw.MolToFile(mol, 'liq_2_with_prop.png')
